da3da94508f4926fcc3e93f21aed3095c83c7ed7
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-05-03 13:40:59 -0500

    route fix

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,12 +6,43 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   
- 
+  try {
+
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+
+  } catch(err) {
+
+    res.status(500).json(err)
+  }
 
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+
+  try {
+
+    const categoryData = await Category.findByPk(req.params.id, {
+
+      include: [{model: Product,
+      attributes: ['product_name', 'price', 'stock'],
+      }]
+
+    });
+
+    if (!categoryData) {
+
+      res.status(404).json({message: 'nothing found with this id. sorry!'});
+      return
+    }
+
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+
+    res.status(500).json(err)
+  }
   
   
 
